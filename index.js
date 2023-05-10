cde575f6c40c5d1c1172a8aab159c741d5633c48
EMMANUEL COOKE <76752588+emmanuelcooke@users.noreply.github.com>
2023-05-10 13:57:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,35 +5,41 @@ const { fetchBitruePrices } = require('./bitrue');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const symbols = [
-	'BTCUSDT',
-	'ETHUSDT',
-	'LTCUSDT',
-	'XRPUSDT',
-	'XLMUSDT',
-	'DOGEUSDT',
-	'ADAUSDT',
-	'ALGOUSDT',
-	'BCHUSDT',
-	//'DGBUSDT',
-	'FILUSDT',
-	'SGBUSDT'
+		'BTCUSDT',
+		'ETHUSDT',
+		'LTCUSDT',
+		'XRPUSDT',
+		'XLMUSDT',
+		'DOGEUSDT',
+		'ADAUSDT',
+		'ALGOUSDT',
+		'BCHUSDT',
+		// 'DGBUSDT',
+		'FILUSDT',
+		'SGBUSDT',
 ];
 
 app.get('/', async (req, res) => {
-	// Information to reach API
-const binancePrices = await fetchBinancePrices(symbols);
-const bitruePrices = await fetchBitruePrices(symbols);});
-const allPrices = {};
-	for (const symbol of symbols) {
-		const binancePrice = binancePrices[symbol];
-		const bitruePrice = bitruePrices[symbol];
+		const binancePrices = await fetchBinancePrices(symbols);
+		const bitruePrices = await fetchBitruePrices(symbols);
 
-		allPrices[symbol] = {
-			binance: binancePrice,	bitrue: bitruePrice
-		};
-	}
-res.json(allPrices);
+		if (!binancePrices || !bitruePrices) {
+				res.status(500).json({ error: 'Failed to fetch prices from Binance or Bitrue' });
+				return;
+		}
 
-app.listen(5000, () => {
-    console.log('listening on port on ${PORT}');
+		const allPrices = {};
+		for (const symbol of symbols) {
+				const binancePrice = binancePrices[symbol];
+				const bitruePrice = bitruePrices[symbol];
+
+						allPrices[symbol] = {
+								binance: binancePrice,	bitrue: bitruePrice
+						};
+		}
+		res.json(allPrices);
+});
+
+app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
 });
