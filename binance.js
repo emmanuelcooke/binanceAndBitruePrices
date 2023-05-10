e901cf7b0519b4d90e6d28bf2ebf53f000307e75
EMMANUEL COOKE <76752588+emmanuelcooke@users.noreply.github.com>
2023-05-10 13:58:34 -0600

    Update binance.js

--- binance.js
+++ binance.js
@@ -1,24 +1,36 @@
 const axios = require('axios');
 const BINANCE_US_API = 'https://api.binance.us/api/v3/ticker/price';
+
 async function fetchBinancePrices(symbols) {
-	try {
-		const prices = {};
-		for (const symbol of symbols) {
-			const response = await axios.get(BINANCE_API, { params: { symbol } });
-			if (!response.data || !response.data.price) {
-				throw new Error(`No data received for symbol ${symbol} from Binance API`);
-			}
-			prices[symbol] = Math.floor(parseFloat(response.data.price * 10 ** 5));
-	}
-		return prices;
+		try {
+				const encodedSymbols = encodeURIComponent(JSON.stringify(symbols));
+				const response = await axios.get(BINANCE_US_API);
+
+				if (!response.data) {
+						throw new Error('No data received from Binance US API');
+				}
+
+				const allPrices = response.data;
+				const prices = {};
+
+				for (const symbol of symbols) {
+						const symbolPrice = allPrices.find((price) => price.symbol === symbol);
+						if (symbolPrice) {
+								prices[symbol] = Math.floor(symbolPrice.price * 10 ** 5);
+						} else {
+								prices[symbol] = 'N/A';
+						}
+				}
+
+				return prices;
 		} catch (error) {
-			console.error(`Error fetching Binance prices: ${error.message}`);
-			console.error('Response data:', error.response?.data);
-			const prices = {};
-			for (const symbol of symbols) {
-				prices[symbol] = 'N/A';
-			}
-			return prices;
+				console.error(`Error fetching Binance US prices: ${error.message}`);
+				console.error('Response data:', error.response?.data);
+				const prices = {};
+				for (const symbol of symbols) {
+						prices[symbol] = 'N/A';
+				}
+				return prices;
 		}
 }
 
